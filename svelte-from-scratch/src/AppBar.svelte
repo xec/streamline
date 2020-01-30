8516fd28a37ac1394b3d78f0f263ffafaf4d9214
Eric Morton <ericbm@yahoo.com>
2020-01-30 23:22:56 +0100

    font awesome

--- svelte-from-scratch/src/AppBar.svelte
+++ svelte-from-scratch/src/AppBar.svelte
@@ -1,19 +1,18 @@
 <script>
   import Dropdown from "./DropDown.svelte";
-  let open = false;
 </script>
 
 <div class="bf-appbar">
   <a href="/" class="logo">svelte-from-scratch</a>
   <nav>
-    <Dropdown bind:open>
-      <button class="bf-button" slot="trigger">{open ? '▾' : '▸'} notifications</button>
+    <Dropdown let:open>
+      <button class="bf-button" slot="trigger">{open ? '▾' : '▸'} Notifications</button>
       <a href="#">Message</a>
       <a href="#">News!</a>
       <a href="#">Some nagging</a>
       <a href="#">Moar nags</a>
     </Dropdown>
-    <Dropdown trigger="profile">
+    <Dropdown trigger="Profile">
       <a href="#">Settings</a>
       <a href="#">Dark mode</a>
       <a href="#">Sign out</a>
--- svelte-from-scratch/src/Dropdown.svelte
+++ svelte-from-scratch/src/Dropdown.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Icon from "svelte-awesome";
+  import { faCaretRight, faCaretDown } from "@fortawesome/free-solid-svg-icons";
   export let open = false;
   export let trigger = "toggle";
   let dropdownElement;
@@ -15,14 +17,17 @@
 
 <svelte:window on:click={windowClick} on:keydown={windowKeydown} />
 <div class="bf-dropdown" bind:this={dropdownElement}>
-  <div class="bf-dropdown-trigger" on:click={() => (open = !open)}>
-    <slot name="trigger">
-      <button class="bf-button">{open ? '▾' : '▸'} {trigger}</button>
+  <div class="bf-dropdown-trigger" on:click={() => open = !open}>
+    <slot name="trigger" open={open}>
+      <button class="bf-button">
+        <Icon data={open ? faCaretDown : faCaretRight} class="fa-fw" />
+        {trigger}
+      </button>
     </slot>
   </div>
   {#if open}
     <div class="bf-dropdown-content">
-      <slot>missing content</slot>
+      <slot>[missing content]</slot>
     </div>
   {/if}
 </div>
