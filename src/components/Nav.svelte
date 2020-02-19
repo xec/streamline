08914d6bdc71202df10d0fd88f3b0b45f3d864ae
Eric Morton <ericbm@yahoo.com>
2020-02-19 20:06:43 +0100

    snowflake tweaks

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,6 +1,12 @@
 <script>
   import Icon from "svelte-awesome";
-  import { faHome, faInfoCircle, faSortAmountUp, faBorderAll, faSnowflake } from "@fortawesome/free-solid-svg-icons";
+  import {
+    faHome,
+    faInfoCircle,
+    faSortAmountUp,
+    faBorderAll,
+    faSnowflake
+  } from "@fortawesome/free-solid-svg-icons";
 
   import AppBar from "../../svelte-from-scratch/src/AppBar.svelte";
   import Navigation from "../../svelte-from-scratch/src/Navigation.svelte";
@@ -12,30 +18,34 @@
 
 <AppBar>
   <DarkmodeToggle />
+  <a href="snowflakes">
+    <Icon data={faSnowflake} />
+  </a>
   <Dropdown trigger="Profile">
     <a href="about" class:active={segment === 'about'}>About</a>
     <a href="counter" class:active={segment === 'counter'}>Counter</a>
   </Dropdown>
 </AppBar>
+
 <Navigation>
   <a href="/" class:bf-active={!segment}>
     <Icon data={faHome} />
     Home
   </a>
-  <a href="about" class:bf-active={segment==='about'}>
+  <a href="about" class:bf-active={segment === 'about'}>
     <Icon data={faInfoCircle} />
     About
   </a>
-  <hr>
-  <a href="counter" class:bf-active={segment==='counter'}>
+  <hr />
+  <a href="counter" class:bf-active={segment === 'counter'}>
     <Icon data={faSortAmountUp} />
     Counter
   </a>
-  <a href="fiveinarow" class:bf-active={segment==='fiveinarow'}>
+  <a href="fiveinarow" class:bf-active={segment === 'fiveinarow'}>
     <Icon data={faBorderAll} />
     Five-in-a-row
   </a>
-  <a href="snowflakes" class:bf-active={segment==='snowflakes'}>
+  <a href="snowflakes" class:bf-active={segment === 'snowflakes'}>
     <Icon data={faSnowflake} />
     Snowflakes
   </a>
--- src/routes/snowflakes.svelte
+++ src/routes/snowflakes.svelte
@@ -1,6 +1,40 @@
 <script>
-import snowflakes from '@xec/snowflakes'
-import '@xec/snowflakes/package/snowflakes.css'
+  import snowflakes from "@xec/snowflakes";
+  import "@xec/snowflakes/package/snowflakes.css";
+  let speed = 1;
+  let count = 90;
+  $: {
+    snowflakes.updateSpeed(speed);
+  }
+  $: {
+    snowflakes.updateCount(count);
+  }
+  function setSpeed(e) {
+    snowflakes.updateSpeed(e.target.value);
+    console.log(e);
+  }
 </script>
 
-<button class="bf-button" on:click={snowflakes.start}>Snow?</button>
+<style>
+  div {
+    margin-top: 1rem;
+  }
+  label {
+    display: inline-block;
+    width: 5rem;
+  }
+</style>
+
+<button class="bf-button" on:click={snowflakes.toggle}>Toggle snow</button>
+
+<div>
+  <label>Count</label>
+  <input type="range" min="1" max="250" bind:value={count} />
+  ({count})
+</div>
+
+<div>
+  <label>Speed</label>
+  <input type="range" min="0.1" max="10" step="0.1" bind:value={speed} />
+  ({speed})
+</div>
