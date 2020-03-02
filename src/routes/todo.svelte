392900e80e3526713a31a109f3d54a85b840ba9d
Eric Morton <ericbm@yahoo.com>
2020-03-02 21:13:40 +0100

    dark mode for the todos

--- src/routes/todo.svelte
+++ src/routes/todo.svelte
@@ -1,5 +1,24 @@
 <script>
-  import Todo from '@xec/svelte-todo'
+  import Todo from "@xec/svelte-todo";
 </script>
 
-<Todo />
+<style>
+  .todoWrapper {
+    --todo-input-text: var(--bfc-base-200);
+    --todo-input-bg: var(--bfc-base-0);
+    --todo-input-border: none;
+    --todo-item-text: #333;
+    --todo-item-bg: hsl(240, 8%, 93%);
+    --todo-item-bg-hover: white;
+    --todo-item-border: 1px solid hsl(240, 8%, 70%);
+    --todo-item-done-bg: hsl(240, 8%, 98%);
+    --todo-item-done-border: 1px solid hsl(240, 8%, 90%);
+    --todo-font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    --todo-remove-bg: black;
+    --todo-remove-text: white;
+  }
+</style>
+
+<div class="todoWrapper">
+  <Todo />
+</div>
