0647e7387e390a4d72b8e3ad1e9958abbd300a42
Eric Morton <ericbm@yahoo.com>
2020-03-08 14:54:05 +0100

    todo dark mode

--- src/routes/todo.svelte
+++ src/routes/todo.svelte
@@ -7,16 +7,21 @@
   .todoWrapper {
     --todo-input-text: var(--bfc-base-200);
     --todo-input-bg: var(--bfc-base-0);
-    --todo-input-border: none;
-    --todo-item-text: #333;
-    --todo-item-bg: hsl(240, 8%, 93%);
-    --todo-item-bg-hover: white;
-    --todo-item-border: 1px solid hsl(240, 8%, 70%);
-    --todo-item-done-bg: hsl(240, 8%, 98%);
-    --todo-item-done-border: 1px solid hsl(240, 8%, 90%);
+    --todo-input-border: 1px solid var(--bfc-base-150);
+    --todo-item-text: var(--bfc-base-200);
+    --todo-item-bg: var(--bfc-base-120);
+    --todo-item-bg-hover: var(--bfc-base-150);
+    --todo-item-border: none;
+    --todo-item-done-bg: var(--bfc-base-110);
+    --todo-item-done-border: none;
     --todo-font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
     --todo-remove-bg: black;
     --todo-remove-text: white;
+  }
+  .todoWrapper :global(.svelte-todo-input) {
+    border: var(--todo-input-border);
+    color: var(--todo-input-text);
+    background-color: var(--todo-input-bg);
   }
 </style>
 
