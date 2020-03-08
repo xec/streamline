9c5ddc7cbb32917eb2db62da6b868abb6faec9ed
Eric Morton <ericbm@yahoo.com>
2020-03-08 16:59:05 +0100

    reactivated page transitions

--- src/components/TransitionWrapper.svelte
+++ src/components/TransitionWrapper.svelte
@@ -2,6 +2,6 @@
   import { fly } from "svelte/transition";
 </script>
 
-<!-- <div in:fly={{ x: 300, duration: 500, delay: 300 }} out:fly={{x: -300, duration: 300}}> -->
+<div in:fly={{ x: 300, duration: 500, delay: 300 }} out:fly={{x: -300, duration: 300}}>
   <slot />
-<!-- </div> -->
+</div>
