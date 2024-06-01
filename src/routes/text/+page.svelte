bad3b0bce00007765898b9faf0f2b68be02ff401
Eric Morton <ericbm@yahoo.com>
2024-06-02 00:13:49 +0200

    text edit page rune experiment

---
+++ src/routes/text/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let color = $state('#bada55')
+</script>
+
+<style>
+  .editBox {
+    font-size: 3rem;
+    padding: 10rem 1rem;
+    text-align: center;
+    outline: none;
+    background-color: var(--bfc-base-0);
+  }
+</style>
+
+<div contenteditable class="editBox" style="color: {color}">Edit me</div>
+<section>
+  <label>Text color: <input type="color" bind:value={color}> {color}</label> 
+</section>
