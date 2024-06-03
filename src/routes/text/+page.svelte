c4399c6b496cf3f361c97c1db0e82fc3c26f8f08
Eric Morton <ericbm@yahoo.com>
2024-06-03 23:54:24 +0200

    text editor font size

--- src/routes/text/+page.svelte
+++ src/routes/text/+page.svelte
@@ -1,28 +1,49 @@
 <script lang="ts">
-	type Stroke = {
-		color: string;
-		width: number;
-	};
+	// const customFont = {name: "custom..."}
+	const fonts = [{ name: 'Montserrat' }, { name: 'Arial' }, { name: '"Mr DaFoe"' }] as const;
+	const cssUnits = ['rem', 'px', 'pt'];
 
+	let selectedFont = $state<(typeof fonts)[number]>(fonts[0]);
+	let fontSize = $state(3);
+	let fontSizeUnit = $state<(typeof cssUnits)[number]>(cssUnits[0]);
 	let color = $state('#bada55');
 	let strokeColor = $state('#000077');
 	let strokeWidth = $state(0);
 
 	let inlineStyle = $derived(`
+  font-family: ${selectedFont.name};
+  font-size: ${fontSize}${fontSizeUnit};
   color: ${color};
   -webkit-text-stroke: ${strokeWidth}px ${strokeColor};
 `);
 </script>
 
 <div contenteditable class="editBox" style={inlineStyle}>Edit me</div>
+
+<section>
+	<h2>Font</h2>
+	<select bind:value={selectedFont}>
+		{#each fonts as font}
+			<option value={font}>{font.name}</option>
+		{/each}
+	</select>
+	<input type="number" min="0" bind:value={fontSize} />
+	<select bind:value={fontSizeUnit}>
+		{#each cssUnits as unit}
+			<option value={unit}>{unit}</option>
+		{/each}
+	</select>
+</section>
 <section>
 	<label>Text color: <input type="color" bind:value={color} /> {color}</label>
 	<div>[rotation]</div>
 </section>
 <section>
 	<h2>Stroke</h2>
-	<label>color: <input type="color" bind:value={strokeColor} /> {strokeColor}</label>
-	<label>width: <input type="number" bind:value={strokeWidth} /> {strokeWidth}</label>
+	<label>stroke color: <input type="color" bind:value={strokeColor} /> {strokeColor}</label>
+	<label>
+		stroke width: <input type="number" min="0" bind:value={strokeWidth} />{strokeWidth}
+	</label>
 </section>
 <section>
 	<h2>CSS</h2>
@@ -31,7 +52,6 @@
 
 <style>
 	.editBox {
-		font-size: 3rem;
 		padding: 10rem 1rem;
 		text-align: center;
 		outline: none;
