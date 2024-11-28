5fa3a6cff867fdfb2496ad9ac44dc39245314f3e
Eric Morton <ericbm@yahoo.com>
2024-11-28 11:04:33 +0100

    Delete old directory

--- old/src/components/TransitionWrapper.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import { fly } from "svelte/transition";
-</script>
-
-<div in:fly|local={{ x: 300, duration: 500, delay: 300 }} out:fly|local={{x: -300, duration: 300}}>
-  <slot />
-</div>
